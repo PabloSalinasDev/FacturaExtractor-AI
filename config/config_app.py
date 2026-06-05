a1bcd386e224888afcd28949076e4dbeff7235ed
PabloSalinas82 <pablosalinas82@gmail.com>
2026-06-05 12:58:11 -0300

    chore: actualizar versión a 1.1.0

--- config/config_app.py
+++ config/config_app.py
@@ -1,4 +1,4 @@
 APP_TITLE = "FacturaExtractor - Gestión de Facturas con IA"
-APP_VERSION = "PyBloSoft © 2026 - Ver. 1.0.0"
+APP_VERSION = "PyBloSoft © 2026 - Ver. 1.1.0"
 
-myappid = 'PyBloSoft.FacturaExtractor.1.0.0'
+myappid = 'PyBloSoft.FacturaExtractor.1.1.0'
